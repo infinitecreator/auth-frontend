1bc2fb1a88303617cb890037067499752cf44356
Niraj Kumar <mtsingh009@gmail.com>
2024-02-20 17:20:47 +0530

    added loading page for delay api requests

--- src/components/common/Redirect.js
+++ src/components/common/Redirect.js
@@ -1,21 +1,22 @@
 import useNavigation from "../../hooks/use-navigation";
 import Logo from "../Logo";
 import GrayText from "./GrayText";
+import Loading from "./Loading";
 import "./Redirect.css" ;
 import { useEffect, useState } from "react";
 
 export default function Redirect({buttonText, buttonUrl, greyText}){
-    const [showComponent, setShowComponent] = useState(false) ;
+    const [showComponent, setShowComponent] = useState(2) ;
     const {navigate} = useNavigation() ;
     useEffect(()=>{
         
         fetch(`${process.env.REACT_APP_PROTOCOL}://${process.env.REACT_APP_BACKEND_URL}/api/users/currentuser`,{ credentials: 'include' })
         .then((res) => {
             if (res.status === 200) {
-                setShowComponent(false) ;
+                setShowComponent(0) ;
                 navigate('/homepage')
             } else {
-                setShowComponent(true) ;
+                setShowComponent(1) ;
             }
         });
         
@@ -29,15 +30,21 @@ export default function Redirect({buttonText, buttonUrl, greyText}){
     }
     return (
         <>
-        { showComponent &&
-            <div className="main-redirect">
-                <Logo text = "AuthHub"/>
-                <GrayText text={greyText}/>
-                {buttonText && <button  onClick = {handleSignin}>{buttonText}</button>}
-
-                {/* <h1>{text}</h1> */}
-
-            </div>
+        { showComponent === 1 ?
+            ( 
+                <div className="main-redirect">
+                    <Logo text = "AuthHub"/>
+                    <GrayText text={greyText}/>
+                    {buttonText && <button  onClick = {handleSignin}>{buttonText}</button>}
+                </div>
+            ) :
+            showComponent === 0 ?
+            (
+                <h1 style={{display: 'flex', justifyContent: 'center', fontSize:'larger'}}>Not Allowed</h1>
+            ) :
+            (
+                <Loading text = "Loading..."/>
+            )
         }
         </>
     )
